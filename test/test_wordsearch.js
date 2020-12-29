67c10fc9f97c6a67b110a93b034f882cc16a2403
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-12-29 17:55:36 +0000

    Intial Commit

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,9 +1,21 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
+  it("should return false if grid is empty", function() {
+    const result = wordSearch([], 'FRANK')
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if grid has empty rows", function() {
+    const result = wordSearch([[]], 'FRANK')
+
+    assert.isFalse(result);
+  });
+
   it("should return false if the word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -20,7 +32,23 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if backward horizontal match is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'Y', 'R', 'R', 'E', 'J', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'JERRY')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if forward horizontal match is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -32,6 +60,68 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if backward vertical match is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'O', 'Y', 'E', 'R', 'L'],
+      ['B', 'Y', 'R', 'M', 'E', 'J', 'Y', 'B'],
+      ['U', 'B', 'T', 'S', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'O', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'C', 'Q', 'U', 'A', 'L'],
+    ], 'COSMO')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if forward vertical match is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'K', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'R', 'R', 'G'],
+      ['W', 'H', 'C', 'O', 'Y', 'A', 'R', 'L'],
+      ['B', 'Y', 'R', 'M', 'E', 'M', 'Y', 'B'],
+      ['U', 'B', 'T', 'S', 'A', 'E', 'A', 'I'],
+      ['O', 'D', 'C', 'O', 'K', 'R', 'A', 'S'],
+      ['E', 'Z', 'K', 'C', 'Q', 'U', 'A', 'L'],
+    ], 'KRAMER')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if backward diagonal match is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'E'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'A', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'I', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'N', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'E', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ], 'ELAINE')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if forward diagonal match is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'E'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'B', 'F', 'Q', 'A', 'A', 'L'],
+      ['H', 'M', 'J', 'E', 'I', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'N', 'N', 'E', 'R', 'L'],
+      ['B', 'F', 'E', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'S', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ], 'BENES')
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,79 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+//Searchs rows for word match
+const rowSearch = (letterGrid, word) => {
+  const wordGrid = letterGrid.map(letterRow => letterRow.join(''));
+
+  for (const wordRow of wordGrid) {
+
+    if (wordRow.includes(word)) {
+      return true;
+    } else if (wordRow.split('').reverse().join('').includes(word)){
+      return true;
     }
+
+  }
+
+  return false;
 }
 
-module.exports = wordSearch
+//Transposes matrix (rows to columns and vice versa) and preps for rowSearch
+//Helps check for vertical matches
+const transpose = (letterGrid) => {
+  const numColumns = letterGrid[0].length;
+  const numRows = letterGrid.length;
+  const transposedLetterGrid = [];
+  
+  for (let column = 0; column < numColumns; column++) {
+    let letterRow = [];
+
+    for (let row = 0; row < numRows; row++) {
+      letterRow.push(letterGrid[row][column]);
+    }
+
+    transposedLetterGrid.push(letterRow);
+  }
+
+  return transposedLetterGrid;
+}
+
+//Extracts diagonals and returns new grid to prep for rowSearch
+const diagonals = (letterGrid) => {
+  const diagonalLetterGrid = [];
+  const l = letterGrid.length;
+  const diagonalOne = [];
+  const diagonalTwo = [];
+
+
+  for (let i = 0; i < l; i++) {
+    diagonalOne.push(letterGrid[i][i]);
+    diagonalTwo.push(letterGrid[l - 1 - i][i])
+  }
+
+  diagonalLetterGrid.push(diagonalOne);
+  diagonalLetterGrid.push(diagonalTwo);
+
+  return diagonalLetterGrid;
+}
+
+//Program runner
+//See test cases in "./test/test_wordsearch.js"
+const wordSearch = (letterGrid, word) => { 
+  if (letterGrid.length && letterGrid[0].length) {
+    if (rowSearch(letterGrid, word)) {
+      return true;
+    }
+
+    if (rowSearch(transpose(letterGrid), word)) {
+      return true;
+    }
+
+    if (letterGrid.length === letterGrid[0].length) {
+      if (rowSearch(diagonals(letterGrid), word)) {
+        return true;
+      }
+    }
+  }
+
+return false;
+}
+
+module.exports = wordSearch;
